e798705424fa8002e561a9e576540a20340af49b
craigrmills <craig.r.mills86@gmail.com>
2021-07-15 10:56:18 -0500

    Created Dinosaur Class

---
+++ herd.py
@@ -0,0 +1,21 @@
+from dinosaur import Dinosaur
+
+
+class Heard:
+
+    def __init__(self):
+        self.dinosaurs = []
+
+    def create_heard(self):
+        i = 0
+        dinosaurs = []
+        dinosaur_names = ["Long-neck", "Trihorn", "Sharp-Tooth"]
+        damage_of_dinosaurs = [15, 25, 35]
+        while i < 3:
+            name = dinosaur_names[i]
+            dino_damage = damage_of_dinosaurs[i]
+            dinosaur = Dinosaur(name, dino_damage)
+            dinosaurs.append(dinosaur)
+            i += 1
+        self.dinosaurs = dinosaurs
+        pass
