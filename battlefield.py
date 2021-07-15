c15aca2dff456c7cf82ac550f8ea6a33cd78ee3a
craigrmills <craig.r.mills86@gmail.com>
2021-07-15 18:29:28 -0500

    Created Battlefield Function

--- battlefield.py
+++ battlefield.py
@@ -1,3 +1,8 @@
+from fleet import Fleet
+from herd import Herd
+import random
+
+
 class Battlefield:
 
     def __init__(self):
@@ -5,19 +10,48 @@ class Battlefield:
         self.herd = None
 
     def run_game(self):
-        pass
+        fleet = Fleet()
+        herd = Herd()
+        first_turn = self.display_welcome()
+        fleet.create_fleet()
+        herd.create_heard()
+        self.fleet = fleet
+        self.herd = herd
+        winner = None
+        game_state = True
+        while game_state:
+            if first_turn == 1:
+                self.robo_turn()
+            else:
+                self.dino_turn()
+            if self.fleet.robots[0].health <= 0:
+                print(self.fleet.robots[0].name + " has exploded!")
+                self.fleet.robots.remove(self.fleet.robots[0])
+            if self.herd.dinosaurs[0].health <= 0:
+                print(self.herd.dinosaurs[0].name + " has met it's end!")
+                self.herd.dinosaurs.remove(self.herd.dinosaurs[0])
+            if len(self.fleet.robots) < 1:
+                winner = "Dinosaurs have Prevailed!"
+                game_state = False
+            if len(self.herd.dinosaurs) < 1:
+                winner = "Robots have Conquered all!"
+                game_state = False
+        self.display_winners(winner)
 
     def display_welcome(self):
-        pass
+        print("Welcome to Robots Vs Dinosaurs!\n Enjoy the Fight!\n")
+        first_turn = random.randrange(1, 2)
+        if first_turn == 1:
+            print("The Robots have won the coin toss and shall go first.\n")
+        else:
+            print("The Dinosaurs have won the coin toss and shall go first.\n")
+        return first_turn
 
-    def battle(self):
-        pass
+    def dino_turn(self):
+        self.herd.dinosaurs[0].attack(self.fleet.robots[0])
 
-    def dino_turn(self, dinosaur):
-        pass
-
-    def robo_turn(self, robot):
-        pass
+    def robo_turn(self):
+        self.fleet.robots[0].attack(self.herd.dinosaurs[0])
 
     def show_dino_opponent_options(self):
         pass
@@ -25,5 +59,5 @@ class Battlefield:
     def show_robo_opponent_options(self):
         pass
 
-    def display_winners(self):
-        pass
+    def display_winners(self, winner):
+        print(winner)
